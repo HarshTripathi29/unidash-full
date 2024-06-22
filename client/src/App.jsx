2ffd58a4f1853663dcb0c162e92289ab24b173ce
Harsh Tripathi <106900613+HarshTripathi29@users.noreply.github.com>
2024-06-22 11:51:26 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
 import Dashboard from './pages/Dashboard';
