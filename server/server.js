7ab7f351e03b43e7001432ad3210343a58676662
Harsh Tripathi <106900613+HarshTripathi29@users.noreply.github.com>
2024-06-22 17:21:50 +0530

    deployment code

--- server/server.js
+++ server/server.js
@@ -6,10 +6,11 @@ const bodyParser = require('body-parser');
 const watchlistRoutes = require('./routes/Watchlist');
 const authRoutes = require('./routes/auth');
 const tokenRoutes = require('./routes/tokens');
+const path = require("path");
 
 const app = express();
 app.use(cors({
-    origin: 'https://unidash-liard.vercel.app, 
+    origin: 'http://localhost:5173', 
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'], 
     allowedHeaders: ['Content-Type', 'Authorization']
   }));
@@ -26,6 +27,27 @@ app.use(bodyParser.json());
 app.use('/api/v1/watchlist', watchlistRoutes);
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/tokens', tokenRoutes);
+
+//-----------------------Deployment--------------
+
+const NODE_ENV = "production";
+const __dirname1 = path.resolve();
+if (NODE_ENV === "production") {
+  // Adjust the path to the client build folder
+  app.use(express.static(path.join(__dirname1, "../client/dist")));
+
+  // Catch-all handler for all other routes, sending back the index.html file
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "../client/dist/index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running successfully");
+  });
+}
+
+//-----------------------Deployment--------------
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
